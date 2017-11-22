56b7107370ca028376c80608c64b5d7fc40c9c42
思月行云 <kenkao_01@126.com>
2017-11-22 15:20:23 +0800

    test the gnbuffers

--- unitest/commtest/main.go
+++ unitest/commtest/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/itfantasy/gonode/components/logger"
+	"github.com/itfantasy/gonode/gnbuffers"
 )
 
 func main() {
-	testTheInterfaceWithNil()
+	//testTheInterfaceWithNil()
+	//testTheByteBuffer()
+	testTheGnBuffers()
 }
 
 type testAStructContainsAnInterface struct {
@@ -17,4 +22,52 @@ type testAStructContainsAnInterface struct {
 func testTheInterfaceWithNil() {
 	var myStruct *testAStructContainsAnInterface = new(testAStructContainsAnInterface)
 	fmt.Println(myStruct.netReporter == nil)
+}
+
+func testTheByteBuffer() {
+	bytess := make([]byte, 1024)
+	bytesBuffer := bytes.NewBuffer(bytess)
+	bytesBuffer.Reset()
+	err := binary.Write(bytesBuffer, binary.BigEndian, (int32)(16))
+	if err != nil {
+		fmt.Println(err)
+	}
+	buffer := ([]byte)("威猛的王大侠")
+	binary.Write(bytesBuffer, binary.BigEndian, (int32)(len(buffer)))
+	err2 := binary.Write(bytesBuffer, binary.BigEndian, (buffer))
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println(bytesBuffer.Bytes())
+	fmt.Println(bytess)
+
+	// -------------- test the reader
+	var value int32
+	binary.Read(bytesBuffer, binary.BigEndian, &value)
+	fmt.Println(value)
+	var lenth int32
+	binary.Read(bytesBuffer, binary.BigEndian, &lenth)
+	fmt.Println(lenth)
+	var bytes []byte = make([]byte, lenth)
+	binary.Read(bytesBuffer, binary.BigEndian, &bytes)
+	fmt.Println(bytes)
+	fmt.Println(string(bytes))
+}
+
+func testTheGnBuffers() {
+	gnbuffer := gnbuffers.BuildBuffer(1024)
+	gnbuffer.PushInt(32)
+	gnbuffer.PushLong(0xAA5555AA)
+	gnbuffer.PushString("威猛的王大侠,     犀利的东哥")
+
+	gnparser := gnbuffers.BuildParser(gnbuffer.Flush(), 0)
+	if val, err := gnparser.Int(); err == nil {
+		fmt.Println(val)
+	}
+	if val, err := gnparser.Long(); err == nil {
+		fmt.Println(val)
+	}
+	if val, err := gnparser.String(); err == nil {
+		fmt.Println(val)
+	}
 }
